4ddcfc55ff02bb08ff818194275aec5409c13039
lstocker327 <lukestocker1@gmail.com>
2021-06-02 19:01:41 -0400

    initialize

---
+++ script.js
@@ -0,0 +1,4 @@
+const plus = document.querySelector("#plus");
+const minus = document.querySelector("#minus");
+const input = document.querySelector("input");
+const h1 = document.querySelector("h1");
