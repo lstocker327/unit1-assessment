56beec6d47d52cac38afae7d43db73ae695dff52
lstocker327 <lukestocker1@gmail.com>
2021-06-02 19:46:48 -0400

    update1

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ const plus = document.querySelector("#plus");
 const minus = document.querySelector("#minus");
 const input = document.querySelector("input");
 const h1 = document.querySelector("h1");
+
+function plusCounter(){
+  counter =  parseInt(h1.innerHTML);
+  counter += parseInt(input.value);
+  h1.innerHTML = counter;
+}
+
+function minusCounter(){
+  counter =  parseInt(h1.innerHTML);
+  counter -= parseInt(input.value);
+  h1.innerHTML = counter;
+}
+
+plus.addEventListener("click", plusCounter);
+minus.addEventListener("click", minusCounter);
