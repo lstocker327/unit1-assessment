4f7afe46fce7559bc9beeb43917d27934efb0b30
lstocker327 <lukestocker1@gmail.com>
2021-06-02 21:10:32 -0400

    final3

--- script.js
+++ script.js
@@ -3,17 +3,47 @@ const minus = document.querySelector("#minus");
 const input = document.querySelector("input");
 const h1 = document.querySelector("h1");
 
+
 function plusCounter(){
-  counter =  parseInt(h1.innerHTML);
-  counter += parseInt(input.value);
+  if (h1.innerHTML.toLowerCase() === "nan"){
+    counter = 0;
+  }else{
+    counter =  parseInt(h1.innerHTML);
+  }
+  if (input.value !== ''){
+    counter += parseInt(input.value);
+  }
+
   h1.innerHTML = counter;
+
+  if (counter < 0){
+    h1.style.color = "red";
+  }
+  if (counter > 0){
+    h1.style.color = "black"
+  }
 }
 
 function minusCounter(){
+  if (h1.innerHTML.toLowerCase() === "nan"){
+    counter = 0;
+  }else{
   counter =  parseInt(h1.innerHTML);
-  counter -= parseInt(input.value);
+  }
+  if (input.value !== ''){
+    counter -= parseInt(input.value);
+  }
+
   h1.innerHTML = counter;
+
+  if (counter < 0){
+    h1.style.color = "red";
+  }
+  if (counter > 0){
+    h1.style.color = "black"
+  }
 }
+
 
 plus.addEventListener("click", plusCounter);
 minus.addEventListener("click", minusCounter);
